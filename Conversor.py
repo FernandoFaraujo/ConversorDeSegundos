5642551248479ec551e82d1c2d772866f9c84bf1
Fernando França Araujo <fernandofaraujo25@gmail.com>
2024-10-19 00:06:21 -0300

    Update Conversor.py

--- Conversor.py
+++ Conversor.py
@@ -1,17 +1,19 @@
-# Este programa apresenta a conersão de segundos em dias, horas, minutos e restante dos segundos.
+def conversão():
+    while True:
+        try:
+        
+            conversor = int(input("\nIndique abaixo quantos segundos quer converter: "))
+            dias = conversor // 86400
+            horas = conversor // 3600
+            segundos = conversor % 3600
+            minutos = segundos // 60
+            mr = segundos // 60
+            sr = minutos % 60
+        
+            print(f"\n{conversor} segundos, resultam em {dias} dias, {horas} horas, {mr} minutos e restam {sr} segundos")
 
-conversor = int(input("\nIndique abaixo quantos segundos quer converter: "))
-
-dias = conversor // 86400
-
-horas = conversor // 3600
-
-segundos = conversor % 3600
-
-minutos = segundos // 60
-
-mr = segundos // 60
-
-sr = minutos % 60
-
-print(f"\n{conversor} segundos, resultam em {dias} dias, {horas} horas, {mr} minutos e restam {sr} segundos.\n")
+    
+        except ValueError:
+            print ("\nletras ou palavras não podem ser executadas, insira novamente abaixo.")
+        
+conversão()
